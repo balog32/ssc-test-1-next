c3f26a661b787fc2dbec3f0ad4d809331b09913f
balog32 <balognorbert81@yahoo.com>
2023-08-12 00:22:18 +0300

    feat:fisier nou

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+
+//button.addEventListner('click',(event)=>){
+//button.style.display ="block";
+
+//};
+ //onclick=(event) =>{
+    //inregistrare.style.display ="block"
+// };
+//button.addEventListener("click" , (event) =>){
+  //  button.intrebare = 'click count: &(inregistrare.style.display="block")';
+    //#inregistrare.style.display="block";
+//};
